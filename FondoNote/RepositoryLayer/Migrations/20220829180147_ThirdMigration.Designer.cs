9a9cbdac1fc48c7abcb1a7799c7d298f293f3bb8
Vishwas <baghel.vishwas0@gmail.com>
2022-08-29 23:37:18 +0530

    [Vishwas]added: Collab Add API Added

--- FondoNote/RepositoryLayer/Migrations/20220829180147_ThirdMigration.Designer.cs
+++ FondoNote/RepositoryLayer/Migrations/20220829180147_ThirdMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RepositoryLayer.Context;
 
 namespace RepositoryLayer.Migrations
 {
     [DbContext(typeof(FundoContext))]
-    partial class FundoContextModelSnapshot : ModelSnapshot
+    [Migration("20220829180147_ThirdMigration")]
+    partial class ThirdMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FondoNote/RepositoryLayer/Migrations/20220829180147_ThirdMigration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class ThirdMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CollabTable",
+                columns: table => new
+                {
+                    CollabId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CollabEmail = table.Column<string>(nullable: true),
+                    UserId = table.Column<long>(nullable: false),
+                    NotesId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CollabTable", x => x.CollabId);
+                    table.ForeignKey(
+                        name: "FK_CollabTable_NotesTable_NotesId",
+                        column: x => x.NotesId,
+                        principalTable: "NotesTable",
+                        principalColumn: "NotesId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CollabTable_UserTable_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserTable",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollabTable_NotesId",
+                table: "CollabTable",
+                column: "NotesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollabTable_UserId",
+                table: "CollabTable",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CollabTable");
+        }
+    }
+}
